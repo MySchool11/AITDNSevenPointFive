77badfa90da942cca9a1368c9a0e25a326b03025
Samuel Bancroft <sam@samuelbancroft.com>
2017-06-03 15:41:22 +0100

    Update Program.cs

--- AITDNSevenPointFive/Program.cs
+++ AITDNSevenPointFive/Program.cs
@@ -15,7 +15,7 @@ namespace AITDNSevenPointFive
     /// <code>
     /// Using Delegates to perform two operations on a pair of strings - imagine these methods are in seperate areas of the program.
     /// A lot of the needed varibles are declared at the start of the file, are they all needed? Could more be used to clarify the code.
-    /// Are there repitions in the code which can be refactored out into seperate methods to apply the DRY principle previously discussed?
+    /// Are there repetitions in the code which can be refactored out into seperate methods to apply the DRY principle previously discussed?
     /// An answer will be forthcoming in the next couple of weeks - no cheating! Good luck.
     /// 
     /// OH and there a no further comments to help you out, you are on your own :-(
